7b071c34931368d9ae19cec92d7207467e01f33b
himisterlee <lejlej100418@gmail.com>
2024-05-27 14:54:25 +0900

    객체 끝

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section08/initblock/Product.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section08/initblock/Product.java
@@ -1,11 +1,13 @@
 package com.ohgiraffers.section08.initblock;
 
+import javax.swing.*;
+
 public class Product {
 
     // 1. 필드를 초기화 하지 않으면 JVM이 정한 기본값으로 객체가 생성된다.
-    // private String name; // 제품명
-    // private int price; // 제품가격
-    // private static String brand; // 제품 브랜드
+//    private String name; // 제품명
+//    private int price; // 제품가격
+//    private static String brand; // 제품 브랜드
 
     // 2. 명시적초기화
     private String name = "갤럭시"; // 제품명
@@ -20,11 +22,11 @@ public class Product {
 
         // 주의사항
         /*
-        * 인스턴스 초기화 블럭에서는 static 필드를 초기화 할 수 있는 것 처럼 보이지만
-        * 이미 static 초기화 블럭은 클래스가 로드될때 이미 기본값으로 초기화를 진행했다.
-        * 따라서 인스턴스 초기화 블럭이 동작하는 시점에는 이미 초기화가 진행된 정적 필드에
-        * 인스턴스 초기화 블럭에서 대입한 값으로 덮어 쓰게 되는 것.
-        * */
+         * 인스턴스 초기화 블럭에서는 static 필드를 초기화 할 수 있는 것 처럼 보이지만
+         * 이미 static 초기화 블럭은 클래스가 로드될때 이미 기본값으로 초기화를 진행했다.
+         * 따라서 인스턴스 초기화 블럭이 동작하는 시점에는 이미 초기화가 진행된 정적 필드에
+         * 인스턴스 초기화 블럭에서 대입한 값으로 덮어 쓰게 되는 것.
+         * */
         brand = "삼성";
 
         System.out.println("인스턴스 초기화 블럭 동작함...");
@@ -32,8 +34,8 @@ public class Product {
 
     static {
         // static 초기화 블럭에서는 non-static 필드를 초기화하지 못한다.
-        // name = "아이폰";
-        // price = 1300000;
+//        name = "아이폰";
+//        price = 1300000;
         brand = "엘지";
         System.out.println("정적 초기화 블럭 동작함...");
     }
@@ -48,7 +50,7 @@ public class Product {
 
     // 필드값 전체 문자열 합치기해서 문자열 반환
     public String getInformation() {
-        return "Product [name = " + this.name + ", price = " +
-                this.price + ", brand = " + Product.brand + "]";
+        return "Product [name = " + this.name +
+                ", price = " + this.price + ", brand = " + Product.brand + "]";
     }
 }
